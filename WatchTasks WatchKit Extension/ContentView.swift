620a25dcba1961d423030f7dd73f6f46aeb49b4c
fbara <fbara1@yahoo.com>
2021-09-13 15:08:12 -0400

    Initial Commit

---
+++ WatchTasks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchTasks WatchKit Extension
+//
+//  Created by Frank Bara on 9/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchTasks WatchKit Extension/WatchTasksApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchTasksApp.swift
+//  WatchTasks WatchKit Extension
+//
+//  Created by Frank Bara on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
